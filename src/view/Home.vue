b2abb686568581709409d5e0f5c2c19e3dfb02ed
axlgtm <fancycyanogen@gmail.com>
2025-05-20 21:32:26 +0700

    home

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <p>Halaman home</p>
+  <div class="flex justify-center h-screen">
+    <div class="flex items-center justify-center flex-col gap-5">
+      <img src="../assets/undraw_bento.svg" alt="Bento" class="w-1/3" />
+      <div>
+        <div class="text-3xl font-semibold text-center">Messages App</div>
+        <p>Send and receive messages online without keeping your phone online.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
